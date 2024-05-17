bcc235c615c600cf9be6ae2c519513a2afc4e63c
n.mayurov <n.mayurov@tinkoff.ru>
2024-05-17 11:41:31 +0300

    fix: first_name column

--- src/main/kotlin/database/UserTable.kt
+++ src/main/kotlin/database/UserTable.kt
@@ -3,12 +3,11 @@ package database
 import org.jetbrains.exposed.sql.ResultRow
 import org.jetbrains.exposed.sql.Table
 import utils.UserDto
-import kotlin.math.log
 
 object UserTable: Table("user") {
     val id = integer("id")
     val login = varchar("login", 20)
-    val firstName = varchar("firstName", 40)
+    val firstName = varchar("first_name", 40)
     val surname = varchar("surname", 40)
     val patronymic = varchar("patronymic", 40).nullable()
 
